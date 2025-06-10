5716a0a577ba85980f2729c7dd81c7f3e131b3c3
Dave <davidjmannion4@gmail.com>
2025-06-10 11:29:16 +0100

    downloader can see what files are downloaded already, also use scraping bee

--- 020_download_match_html.py
+++ 020_download_match_html.py
@@ -1,9 +1,11 @@
-from bs4 import BeautifulSoup
-import pandas as pd
-import requests
+
 import time
 import glob
 import os
+import random
+from scrapingbee import ScrapingBeeClient
+import datetime
+
 # 020_download_match_html.py
 # This script downloads match HTML files from the Football Lineups website based on match codes.
 # It reads match codes from a text file, constructs URLs for each match, and downloads the HTML content.
@@ -25,18 +27,29 @@ def read_text_file_to_list(filepath):
     f.close()
     return [line.strip() for line in lines]
 
+def get_current_time():
+    """
+    Get the current time in a formatted string.
+    """
+    return datetime.datetime.now().strftime("%H:%M:%S %d-%m-%Y")
 
 def download_match_html(urls,time_delay=20):
-    headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
-    }
 
-    print('Downloading Match HTML Files...')
+    if len(urls) == 0:
+        print(f"{get_current_time()} No URLs to download. Exiting.")
+        return None
+
+    API_KEY = 'TAKN8UIR1O36S12TWBADOT7ULZPBQ23YR9YB7REMVFWLS833MF4B79RBRQC6AEXDOJZXB2C45KB8CILN' 
+    client = ScrapingBeeClient(api_key=API_KEY)
+
+    print(f'{get_current_time()} Downloading Match HTML Files...')
 
     for url in urls:
-        print(f"Processing URL: {url}")
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        print(f"{get_current_time()} Processing URL: {url}")
+        response = client.get(url,params={
+        'premium_proxy': 'True',
+        'render_js':'False'
+        })
 
         match_code = url.split("/")[-2]  # Extract match code from URL
 
@@ -46,27 +59,55 @@ def download_match_html(urls,time_delay=20):
             with open(f"data/football_lineups/downloaded_lineup_html_files/match_data_{match_code}.html", "w", encoding="utf-8") as file:
                 file.write(response.text)
                 file.close()
-            print(f"HTML for match {match_code} saved successfully.")
+            print(f"HTML for match {match_code} saved successfully.\n")
         else:
             print(f"Failed to retrieve the page. Status code: {response.status_code}")
         print(f"Waiting for {time_delay} seconds before the next request...")
-        time.sleep(time_delay)  # Delay to avoid overwhelming the server
-    print("\n\nAll match HTML files downloaded successfully :).")
+
+        randomised_time_delay = random.randint(time_delay-2, time_delay+2)  # Random delay between 10 and 30 seconds
+        print(f"Next request will be delayed by {randomised_time_delay} seconds.")
+        time.sleep(randomised_time_delay)  
+
+    print(f"\n\n {get_current_time()} All match HTML files downloaded successfully :).")
 
 def get_all_match_codes(filepath):
     """
     Get all match codes from the text file.
     """
     all_match_codes = []
-    for filename in glob.glob(os.path.join(filepath, '*.txt')): #only process .JSON files in folder. 
+    for filename in glob.glob(os.path.join(filepath, '*.txt')): 
         match_codes = read_text_file_to_list(filename)
         all_match_codes += match_codes
 
     return all_match_codes
 
-match_codes = get_all_match_codes('data/football_lineups/extracted_match_numbers/')
-urls = [create_match_url(code) for code in match_codes]
+def get_match_codes_to_be_downloaded(filepath):
+    """
+    Get match codes that have not been downloaded yet.
+    """
+    all_match_codes = get_all_match_codes(filepath)
+    print(len(all_match_codes))
+    match_codes_to_download = []
+
+    for match_code in all_match_codes:
+        try:
+            with open(f"data/football_lineups/downloaded_lineup_html_files/match_data_{match_code}.html", "r", encoding="utf-8") as html_file:
+                html_content = html_file.read()
+                if 'Lineups' in html_content:
+                    print(f"Match code {match_code} already downloaded.")
+                else:
+                    print(f"Match code {match_code} not downloaded or has no lineups. Adding to download list.")
+                    match_codes_to_download.append(match_code)    
+        except FileNotFoundError:
+            print(f"Match code {match_code} not found. Adding to download list.")
+            match_codes_to_download.append(match_code)
+    
+    return match_codes_to_download
+
+
+match_codes_to_be_downloaded = get_match_codes_to_be_downloaded('data/football_lineups/extracted_match_codes')
+urls = [create_match_url(code) for code in match_codes_to_be_downloaded]
 
 print(f'Downloading from {len(urls)} URLs...')
 
-download_match_html(urls, time_delay=31)
+download_match_html(urls, time_delay=15)
