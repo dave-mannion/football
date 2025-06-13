8e0b19717123284182a703108936bb0d7613f990
Dave <davidjmannion4@gmail.com>
2025-06-13 18:11:05 +0100

    player merging working, first basic model, not too bad

--- 110_create_fixture_table.py
+++ 110_create_fixture_table.py
@@ -23,8 +23,8 @@ def format_match_lineup_json(match_lineup_json):
     lineups = match_lineup_json_copy.pop('lineups')
 
     starter_ids = extract_starter_ids(lineups)
-    home_starters = {f'home_starter_{i+1}':x for i,x in enumerate(starter_ids[home_team])}
-    away_starters = {f'away_starter_{i+1}':x for i,x in enumerate(starter_ids[away_team])}
+    home_starters = {f'home_starter_id_fl_{i+1}':x for i,x in enumerate(starter_ids[home_team])}
+    away_starters = {f'away_starter_id_fl_{i+1}':x for i,x in enumerate(starter_ids[away_team])}
 
     match_lineup_json_copy = match_lineup_json_copy | home_starters
     match_lineup_json_copy = match_lineup_json_copy | away_starters
